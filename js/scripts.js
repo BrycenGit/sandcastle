c99f2d5c98710cca2da3f0585b1b8327c95d448b
[brycen] <[brycenbartolome@gmail.com]>
2020-08-11 11:02:58 -0700

    add content to scripts.js add toggle
    co-authored-by: Drew Lockhart <drewmlockhart@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-  $("p").click(function() {
+  $(".clickable1").click(function() {
     $(".text-showing").toggle();
     $(".text-hidden").toggle();
+
   });
+  $(".clickable2").click(function() {
+    $(".img-showing").toggle();
+    $(".img-hidden").toggle();
+  });
+
 });
